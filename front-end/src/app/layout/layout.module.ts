3142266288b17018d76320dcdb16f1354f7cf491
Anton  Golovachev <anotn@antonweb.co.il>
2022-05-05 19:30:04 +0300

    website skeleton

---
+++ front-end/src/app/layout/layout.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import { MainComponent } from './main/main.component';
+import {MatCardModule} from '@angular/material/card';
+
+
+@NgModule({
+  declarations: [
+    NavBarComponent,
+    MainComponent
+  ],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule
+  ],
+  exports: [
+    NavBarComponent,
+    MainComponent
+  ]
+})
+export class LayoutModule { }
